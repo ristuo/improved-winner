540886a407bb199783f6cffb38f6a67730a0433c
risto <risto.tuomainen1@gmail.com>
2018-08-07 21:42:24 +0300

    Use two dataset

--- src/main/R/events/events_by_game.R
+++ src/main/R/events/events_by_game.R
@@ -15,7 +15,7 @@ plot_trace <- function(res, variable, which_to_plot) {
   traceplot(res, paste0(variable, "[", which_to_plot, "]"))
 }
 
-events_years <- c(2018, 2017, 2016 2015)
+events_years <- c(2018, 2017, 2016)
 events_files <- paste0("data/events/events_", events_years, ".csv")
 
 events <- events_files %>% Map(fread, .) %>% rbindlist(use.names = T) %>% as.tbl
@@ -47,16 +47,14 @@ set_goals <- function(df) {
       )
       potential_specials <- filter(potentials, event_type != "Laukaus")
       potential_laukaus <- filter(potentials, event_type == "Laukaus") 
-      if (nrow(potential_specials)) {
+      if (nrow(potential_laukaus)) {
+        index <- potential_laukaus$index[1]
+        row <- nrow(ei_maalit) - (index - 1)
+        ei_maalit$score[row] <- 1       
+      } else if (nrow(potential_specials)) {
         index <- potential_specials$index[1]
         row <- nrow(ei_maalit) - (index - 1)
         ei_maalit$score[row] <- 1
-      } else if (nrow(potential_laukaus)) {
-        if (nrow(potential_laukaus)) {
-          index <- potential_laukaus$index[1]
-          row <- nrow(ei_maalit) - (index - 1)
-          ei_maalit$score[row] <- 1       
-        }
       } else {
         res <- as.data.frame(maali, stringsAsFactors = FALSE) 
         res$event_type <- NA
@@ -79,18 +77,82 @@ goals <- goal_events %>%
   ungroup %>%
   mutate(team = gsub("\\(|\\)", "", team)) 
 
-player_goals <- goals %>%
+event_game <- goals %>%
   mutate(event_type = ifelse(is.na(event_type), "unknown", event_type)) %>%
-  group_by(player_id, event_type) %>%
-  summarize(goals = sum(score), n = n())
+  select(player_id, game_id, event_type, score) %>%
+  mutate(n = 1)
 
+lineup_files <- paste0("data/lineups/2018-08-01/", 2016:2018, ".csv")
+lineups <- Map(function(d) { fread(d) }, lineup_files) %>% rbindlist %>% as.tbl
+missing_lineups <- lineups %>% group_by(game_id) %>% summarize(n = n()) %>% filter(n != 22)
+lineups <- filter(lineups, !game_id %in% missing_lineups$game_id) %>%
+  filter(game_id %in% event_game$game_id)
+lineup_game <- lineups %>%
+  rowwise() %>%
+  do((function(df) {
+    res <- as.data.frame(df, stringsAsFactors = F)[rep(1,4),]
+    res$event_type <- c("Laukaus", "Vapaapotku", "Kulmapotku", "unknown")
+    res$score <- 0
+    res$n <- 0
+    res
+  })(.)) %>%
+  select(game_id, player_id, event_type, score,n) %>%
+  ungroup %>%
+  mutate(event_type = enc2utf8(event_type))
+
+player_goals <- filter(event_game, game_id %in% lineup_game$game_id) %>%
+  bind_rows(lineup_game) %>%
+  group_by(player_id, event_type) %>%
+  summarize(goals = sum(score), n = sum(n), games = n_distinct(game_id))
+
+shot_goals <- filter(player_goals, event_type == "Laukaus")
+other_goals <- filter(player_goals, event_type != "Laukaus") %>%
+  group_by(player_id) %>%
+  summarize(goals = sum(goals), games = sum(games))
+
+set_index <- function(df, variable, indices = c()) {
+  df <- as.data.frame(df)
+  values <- unique(df[,variable])
+  if (!length(indices)) {
+    indeces <- 1:length(values) %>% setNames(values)
+  }
+  res_name <- paste0(variable, "_index")
+  df[,res_name] <- indeces[as.character(df[,variable])]
+  list(df = as.tbl(df), indices = indices)
+}
+
+shot_goals %<>% filter(player_id %in% other_goals$player_id)
+other_goals %<>% filter(player_id %in% shot_goals$player_id)
+shot_index <- set_index(shot_goals, "player_id")
+shot_goals <- shot_index$df
+player_index <- shot_index$indices
+other_goals <- set_index(other_goals, c("player_id"), indices = player_index)[[1]]
 stan_data <- 
   list(
-    n_rows = nrow(player_goals),
-    n_players = length(unique(player_goals$player_id)),
-    player_id_index = player_goals$player_id_index,   
-    goals = player_goals$goals,
-    n = player_goals$n
+    shot_n_rows = nrow(shot_goals),
+    shot_player_id_index = shot_goals$player_id_index,   
+    shot_goals = shot_goals$goals,
+    shot_games = shot_goals$games,
+    shot_n = shot_goals$n,
+    other_player_id_index = other_goals$player_id_index,
+    other_goals = other_goals$goals,
+    other_games = other_goals$games,
+    other_n_rows = nrow(other_goals)
   )
-  
-  
+res <- 
+  stan(  
+    "src/main/R/events/model.stan",
+    data = stan_data,
+    refresh = 1,
+    iter = 5000,
+    chains = 4,   
+    control = list(
+      adapt_delta = 0.99,
+      max_treedepth = 16
+    )
+)
+ss <- rstan::extract(res, "opportunity_lambda")[[1]]
+means <- colSums(ss) / nrow(ss)
+ps <- rstan::extract(res, "p")[[1]]
+pmeans <- colSums(ps) / nrow(ps)
+os <- rstan::extract(res, "other_p")[[1]]
--- src/main/R/events/model.stan
+++ src/main/R/events/model.stan
@@ -1,39 +1,64 @@
 data {
-  int<lower=0> n_rows;
-  int<lower=0> n[n_rows];
-  int<lower=0> goals[n_rows];
-  int<lower=0> player_indices[n_rows];
+  int<lower=0> shot_n_rows;
+  int<lower=0> shot_n[shot_n_rows];
+  int<lower=0> shot_goals[shot_n_rows];
+  int<lower=0> shot_player_id_index[shot_n_rows];
+  vector[shot_n_rows] shot_games;
+  int<lower=0> other_n_rows;
+  int<lower=0> other_goals[other_n_rows];
+  int<lower=0> other_player_id_index[other_n_rows];
+  int other_games[other_n_rows];
 }
 
 parameters {
-  real<lower=0, upper=1> p[n_rows];
-  real<lower=0> lambda[n_rows]
-}
-
-transformed parameters {
-  vector scoring_strength;
-  vector opportunity_strength;
-  scoring_strength = 
-    scoring_strength_mu + 
-    scoring_strength_sigma * raw_scoring_strength
-  opportunity_strength = 
-    opportunity_strength_mu +
-    opportunity_strength_sigma * raw_opportunity_strength;
-}
-
-model {
-  vector[n_rows] raw_scoring_strength;  
-  vector[n_rows] raw_opportunity_strength;
   real scoring_strength_mu;
   real scoring_strength_sigma;
   real opportunity_strength_mu;
   real opportunity_strength_sigma;
-  raw_pss ~ normal(0,1);
-  raw_pos ~ normal(0,1);
+  real other_strength_mu;
+  real other_strength_sigma;
+  vector[shot_n_rows] raw_scoring_strength;  
+  vector[shot_n_rows] raw_opportunity_strength;
+  vector[other_n_rows] raw_other_strength;
+}
+
+transformed parameters {
+  vector[shot_n_rows] scoring_strength;
+  vector[shot_n_rows] opportunity_strength;
+  vector[shot_n_rows] opportunity_lambda;
+  vector[other_n_rows] other_strength;
+  vector[shot_n_rows] p;  
+  vector[other_n_rows] other_p;
+  other_strength  = 
+    other_strength_mu + 
+    raw_other_strength * 
+    other_strength_sigma;
+  other_p = inv_logit(other_strength[other_player_id_index]);
+  scoring_strength = 
+    scoring_strength_mu + 
+    scoring_strength_sigma * raw_scoring_strength;
+  p = inv_logit(scoring_strength[shot_player_id_index]);
+  opportunity_strength = 
+    opportunity_strength_mu +
+    opportunity_strength_sigma * raw_opportunity_strength;
+  opportunity_lambda = exp(opportunity_strength[shot_player_id_index]);
+}
+
+model {
+  raw_scoring_strength ~ normal(0,1);
+  raw_opportunity_strength ~ normal(0,1);
+  raw_other_strength ~ normal(0,1);
+  other_strength_mu ~ normal(0, 31); 
+  other_strength_sigma ~ normal(0, 31);
   opportunity_strength_mu ~ normal(0, 31);
   opportunity_strength_sigma ~ normal(0, 31);
   scoring_strength_mu ~ normal(0, 31);
   scoring_strength_sigma ~ normal(0, 31);
-  n ~ poisson_log(opportunity_strength);
-  goals ~ binomial(n, inv_logit(scoring_strength));
+  shot_n ~ poisson(shot_games[shot_player_id_index] .* opportunity_lambda);
+  shot_goals ~ binomial(shot_n, inv_logit(scoring_strength[shot_player_id_index]));
+  other_goals ~ binomial(other_games, inv_logit(other_strength[other_player_id_index]));
+}
+
+generated quantities {
+
 }
