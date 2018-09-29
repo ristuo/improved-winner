f6382889a7e41fba8d6ff13a2b664f2dbfe79b26
risto <risto.tuomainen1@gmail.com>
2018-09-29 11:52:53 +0300

    Load data from DB

---
+++ src/main/R/storage.R
@@ -0,0 +1,105 @@
+library(RPostgreSQL)
+library(dplyr)
+library(magrittr)
+library(configr)
+
+DEFAULT_PATH <- "/home/risto/Code/hobbies/veikkausliiga/secrets/db-config.yaml"
+
+load_conf_from_env <- function() {
+  username <- Sys.getenv("DB_USER")
+  password <- Sys.getenv("DB_PASSWORD")
+  host <- Sys.getenv("DB_HOST")
+  dbname <- Sys.getenv("DB_NAME")
+  list(
+    username = username,
+    password = password,
+    host = host,
+    dbname = dbname
+  )
+}
+
+load_conf_from_file <- function() {
+  read.config(file=DEFAULT_PATH)$db
+}
+
+load_db_conf <- function() {
+  load_conf_from_file()
+}
+
+get_connection <- function() {
+  drv <- dbDriver("PostgreSQL")
+  conf <- load_db_conf()
+  con <- dbConnect(
+    drv, 
+    dbname = conf$dbname,
+    host = conf$host, 
+    port = conf$port,
+    user = conf$username, 
+    password = conf$password
+  )
+  con
+}
+
+load_games <- function(sport_name, tournament) {
+  con <- get_connection()
+  qs <- paste0("
+    SELECT 
+      *
+    FROM
+      games
+    WHERE
+      sport_name='",sport_name,"' AND 
+      tournament='",tournament,"'
+  ")   
+  res <- dbGetQuery(con, qs) %>% as.tbl
+  games <- filter(res, !is.na(home_team_goals)) 
+  oos <- filter(res, is.na(home_team_goals)) %>%
+    arrange(game_date) %>%
+    filter(game_date >= Sys.Date())
+  dbDisconnect(con)  
+  list(
+    games=games,
+    oos=oos
+  )
+}
+
+load_player_stats <- function(tournament) {
+  if (tournament != "Veikkausliiga") {
+    stop("Only veikkausliiga implemented")
+  }
+  con <- get_connection()
+  qs <- paste0("
+    SELECT 
+      sum(shots) as shots,
+      sum(goals) as goals,
+      sum(games) as games,
+      sum(play_time) as play_time,
+      nimi,
+      player_id
+    FROM
+      veikkausliiga_player_stats 
+    GROUP BY
+      nimi,
+      player_id
+  ")   
+  res <- dbGetQuery(con, qs)
+  dbDisconnect(con)
+  as.tbl(res)
+}
+
+load_lineups <- function(tournament) {
+  con <- get_connection()
+  qs <- paste0("
+    SELECT 
+      team,
+      game_id,
+      player_id,
+      season,
+      game_set
+    FROM
+      lineups 
+  ")   
+  res <- dbGetQuery(con, qs)
+  dbDisconnect(con)
+  as.tbl(res)  
+}
